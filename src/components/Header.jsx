9868fb40c8f7f9b2d167a6737f47e17573cb60db
jnchiarot <140640440+jnchiarot@users.noreply.github.com>
2024-01-14 22:01:39 -0330

    Title edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="main-header">
       <img src="./images/image-from-rawpixel-id-9779147-original.png" />
-      <h1 className="header-title">www.zinearchive.com</h1>
+      <h1 className="header-title">www.zinearchives.com</h1>
     </div>
   );
 };
